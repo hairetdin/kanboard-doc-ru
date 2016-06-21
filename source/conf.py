ce40a53cccafc05c19d4f03f374e3b33b13c2b90
Hairetdinov <amir.hairetdinov@gmail.com>
2016-06-21 17:41:04 +0500

    freebsd-installation translated

--- source/conf.py
+++ source/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Kanboard russian'
-copyright = u'2016, hairetdin'
+copyright = u'2016, Kanboard.ru'
 author = u'hairetdin'
 
 # The version info for the project you're documenting, acts as replacement for
