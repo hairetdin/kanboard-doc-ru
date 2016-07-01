cbc80b0981cf0d20dfb615290c35fbdba279bab4
Hairetdinov <amir.hairetdinov@gmail.com>
2016-07-01 11:12:35 +0500

    translations translated

--- convert-markdown-to-rst.py
+++ convert-markdown-to-rst.py
@@ -5,11 +5,16 @@ import pypandoc
 source_dir = os.getcwd() + '/source'
 cwd = os.chdir(source_dir)
 
+pdoc_args = ['--no-wrap']
+
 for file_name in os.listdir(source_dir):
     file_name_split = file_name.split('.')
     print('Source file: ' + file_name)
     if len(file_name_split) == 2:
         if file_name_split[1] == 'markdown':
             rst_file_name = (file_name_split[0]+'.rst')
-            output = pypandoc.convert(file_name, 'rst', format='md', outputfile=(file_name_split[0]+'.rst'))
+            output = pypandoc.convert(file_name, 'rst', 
+                format='md',
+                outputfile=(file_name_split[0]+'.rst'),
+                extra_args=pdoc_args)
             print(source_dir + '/' + rst_file_name + ' ' + 'created')
